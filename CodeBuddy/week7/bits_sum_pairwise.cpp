9c460baae850bfd58a7a11a4e2c8415b82ee068c
vrn25 <videhraj29072000@gmail.com>
2020-01-22 10:33:47 +0530

    added week7 IEEE CB

---
+++ CodeBuddy/week7/bits_sum_pairwise.cpp
@@ -0,0 +1,15 @@
+int Solution::cntBits(vector<int> &A) {
+    long long count = 0;
+    long long M = 1000000007;
+    for(int i=0;i<31;i++){
+        long long ones = 0, zeros = 0;
+        for(int j=0;j<A.size();j++){
+            if(A[j] & (1<<i))
+                ones++;
+            else
+                zeros++;
+        }
+        count += ones * zeros;
+    }
+    return (count * 2) % M;
+}
